3a2a62a01e227cdb0a7af3b7bffc99ce22385ae7
Ganesh K <ganeshkannan627@gmail.com>
2024-08-06 22:14:24 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# selenium-practice-june2024
---
+++ src/test/java/seleniumpractice/BrowserCommands.java
@@ -0,0 +1,28 @@
+package seleniumpractice;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class NavigationCommands {
+    public static void main(String[] args) {
+
+        // Opening browser - chrome
+        WebDriver driver = new ChromeDriver();
+
+        // Open Google Application
+        driver.get("https://www.google.com/");
+        // Open Facebook Application
+        driver.get("https://www.facebook.com/");
+//        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+
+        // Navigation Commands
+        driver.navigate().back();
+        driver.navigate().forward();
+        driver.navigate().refresh();
+        
+        // Close
+//        driver.close();
+        driver.quit();
+
+    }
+}
